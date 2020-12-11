fd7f0bdee1f75d4f458151e2bd2f8b22a3a870e5
Harvey Lin <chenghaolin0107@gmail.com>
2020-12-11 12:49:02 -0500

    add more cocktails

--- src/cocktails.js
+++ src/cocktails.js
@@ -28,6 +28,36 @@ export const cocktails = [
       id: 'https://static.vinepair.com/wp-content/uploads/2020/04/americano_internal.jpg',
       name: 'Americano',
       ingredients: 'Campari, vermouth, soda water',
+    },
+    {
+      id: 'https://magazine.nd.edu/assets/346661/800x450/aviary_banner.jpg',
+      name: 'Michelada',
+      ingredients: 'Clamato juice, lime juice, Worcestershire sauce, hot sauce, Mexican lager beer',
+    },
+    {
+      id: 'https://www.flairprojectsb.com/wp-content/uploads/2017/07/easy-watermelon-cocktail-800x450.jpg',
+      name: 'watermelon cocktail',
+      ingredients: 'Watermelon, vodka, triple sec, limes juiced',
+    },
+    {
+      id: 'https://pestanacr7lifestyle.com/wp-content/uploads/2018/10/cover-800x450.jpg',
+      name: 'Mango Tango',
+      ingredients: 'Dark rum, coconut rum,  mango nectar, Triple Sec, pineapple juice',
+    },
+    {
+      id: 'https://cf.ltkcdn.net/cocktails/images/std/252630-800x450-colorful-martinis.jpg',
+      name: 'Colorful Martinis',
+      ingredients: 'Gin, Ginger liqueur, Simple syrup, b’Lure butterfly pea flower extract, Fresh lemon juice',
+    },
+    {
+      id: 'https://www.tampabay.com/resizer/UKBICnoA5yBjHA3gX-BosEtHlnI=/800x450/smart/cloudfront-us-east-1.images.arcpublishing.com/tbt/GJMFSIGGNAI6TCHRIBWI6S7HAY.jpg',
+      name: 'Piña colada',
+      ingredients: 'Coconut cream,  pineapple juice, white rum, cherry, pineapple slice',
+    },
+    {
+      id: 'https://i.pinimg.com/originals/b3/e0/83/b3e083e0e7ad8cab11e8110598072917.jpg',
+      name: 'Lava Flow',
+      ingredients: 'light rum, banana, Malibu coconut rum, coconut cream, pineapple juice, pineapple wedge, ice',
     }
   ];
   
