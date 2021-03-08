9f638913a5c18fbf841d4740139dfee65f62a059
Harvey Lin <chenghaolin0107@gmail.com>
2021-03-08 12:32:57 -0500

    added items

--- src/cocktails.js
+++ src/cocktails.js
@@ -16,32 +16,32 @@ export const cocktails = [
       ingredients: 'Gin, Ginger liqueur, Simple syrup, b’Lure butterfly pea flower extract, Fresh lemon juice',
     },
     {
-      id: 'https://i.insider.com/5af59349965f4c30008b47b4?width=1136&format=jpeg',
+      id: 'https://i3w4y2b7.rocketcdn.me/wp-content/uploads/2020/12/A-medical-treatment.png',
       name: 'Gin Tonic',
       ingredients: 'gin, tonicwater, lime',
     },
     {
-      id: 'https://alushlifemanual.com/wp-content/uploads/2020/04/Boulevardier-1-1024x683.jpg',
+      id: 'https://d38trduahtodj3.cloudfront.net/images.ashx?t=ig&rid=ExperienceWeatherford&i=whiskey-sour-cocktail-800x800.jpg',
       name: 'Boulevardier',
       ingredients: 'amaro, sweet vermouth, orange',
     },
     {
-      id: 'https://recipecontent.fooby.ch/13463_3-2_1920-1280.jpg',
+      id: 'https://www.barchick.com/www/media/wysiwyg/vanilla%20chai%20old%20fashioned.png',
       name: 'Old Fashioned',
       ingredients: 'bourbon, sugar cube, Angostura bitters, orange',
     },
     {
-      id: 'https://static.vinepair.com/wp-content/uploads/2020/04/sidecar_internal.jpg',
+      id: 'https://o.aolcdn.com/images/dims3/GLOB/crop/2330x1313+0+0/resize/800x450!/format/jpg/quality/85/http://o.aolcdn.com/hss/storage/midas/83de1e488f604d51f081d197dca922d1/205556991/15_Sidecar-2.jpg',
       name: 'Sidecar',
       ingredients: 'voldka, vermouth, lime',
     },
     {
-      id: 'https://static.vinepair.com/wp-content/uploads/2020/04/vesper_internal.jpg',
+      id: 'https://images.squaremeal.co.uk/cloud/recipes/215/images/square-meals-vodka-martini-248_28052020040831.jpg?w=800&h=450&fit=crop',
       name: 'Vesper',
       ingredients: 'brandy, lemon, triple sec',
     },
     {
-      id: 'https://static.vinepair.com/wp-content/uploads/2020/04/americano_internal.jpg',
+      id: 'https://o.aolcdn.com/images/dims3/GLOB/crop/848x478+284+0/resize/800x450!/format/jpg/quality/85/http://o.aolcdn.com/hss/storage/midas/f52f0ac2cd849a9e2e5abcf0ea4e2729/204695594/Celebrating+around+the+World+1920+x+670.jpg',
       name: 'Americano',
       ingredients: 'Campari, vermouth, soda water',
     },
@@ -54,6 +54,16 @@ export const cocktails = [
       id: 'https://www.tampabay.com/resizer/UKBICnoA5yBjHA3gX-BosEtHlnI=/800x450/smart/cloudfront-us-east-1.images.arcpublishing.com/tbt/GJMFSIGGNAI6TCHRIBWI6S7HAY.jpg',
       name: 'Piña colada',
       ingredients: 'Coconut cream,  pineapple juice, white rum, cherry, pineapple slice',
+    },
+    {
+      id: 'https://images-gmi-pmc.edge-generalmills.com/bf2fda90-219a-4491-a9e7-e3c27bfa7e59.jpg',
+      name: 'Cosmopolitan',
+      ingredients: 'Vodka Citron,   Cranberry juice, Cointreau, Fresh lime juice',
+    },
+    {
+      id: 'https://i.pinimg.com/originals/73/83/b4/7383b408d3b493faad83cdd97e5176ab.jpg',
+      name: 'Margarita',
+      ingredients: 'Tequila, Lime juice, Cointreau',
     },
     {
       id: 'https://i.pinimg.com/originals/b3/e0/83/b3e083e0e7ad8cab11e8110598072917.jpg',
